522bf882bae4fe044bd3d4323993526e546b54c8
keanmayvodoi44 <33352648+keanmayvodoi44@users.noreply.github.com>
2023-09-12 12:58:49 +0700

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import 'package:permission/permission.dart';
@@ -16,7 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -37,9 +34,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-
-  bool _isAllow = false;
-
   @override
   void initState() {
     // TODO: implement initState
@@ -52,129 +46,80 @@ class HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  Widget _buildItem(String text, GestureTapCallback onTap) {
+    return InkWell(
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+      ),
+      onTap: onTap,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Plugin example app'),
       ),
-      body: Center(
-          child: _isAllow?Text("Allowed Location Permission"):MaterialButton(
-              child: Text(
-                  "Request Location Permission"
-              ),
-              onPressed: () async {
-                await PermissionRequest.request(PermissionRequestType.CAMERA, (){});
-                await _checkLocationPermission();
-                // _openSetting();
-                // _checkLocationPermission();
-                // _isAllow = await PermissionRequest.request(PermissionRequestType.LOCATION, (){
-                //   showDialog(
-                //       context: context,
-                //       builder: (_){
-                //         return AlertDialog(
-                //           title: Text("Allowed to access"),
-                //           content: Text("Select Settings to App Information, select (Permissions), enable access and re-enter this screen to use Storage"),
-                //           actions: [
-                //             TextButton(
-                //                 child: Text("Allow"),
-                //                 onPressed: (){
-                //                   Navigator.of(context).pop();
-                //                   PermissionRequest.openSetting();
-                //                 }
-                //             )
-                //           ],
-                //         );
-                //       }
-                //   );
-                // });
-                // setState(() {});
-              }
-          )
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildItem(
+                "Check Camera", () => _check(PermissionRequestType.CAMERA)),
+            _buildItem(
+                "Request Camera", () => _request(PermissionRequestType.CAMERA)),
+            _buildItem(
+                "Check Location", () => _check(PermissionRequestType.LOCATION)),
+            _buildItem("Request Location",
+                () => _request(PermissionRequestType.LOCATION)),
+            _buildItem("Check BackgroundLocation",
+                () => _check(PermissionRequestType.BACKGROUND_LOCATION)),
+            _buildItem("Request BackgroundLocation",
+                () => _request(PermissionRequestType.BACKGROUND_LOCATION)),
+            _buildItem(
+                "Check Storage", () => _check(PermissionRequestType.STORAGE)),
+            _buildItem("Request Storage",
+                () => _request(PermissionRequestType.STORAGE)),
+            _buildItem("Check Notification",
+                () => _check(PermissionRequestType.NOTIFICATION)),
+            _buildItem("Request Notification",
+                () => _request(PermissionRequestType.NOTIFICATION)),
+            _buildItem("Check Microphone",
+                () => _check(PermissionRequestType.MICROPHONE)),
+            _buildItem("Request Microphone",
+                () => _request(PermissionRequestType.MICROPHONE)),
+          ],
+        ),
       ),
     );
   }
 
-  void _openSetting() {
+  _check(PermissionRequestType type) async {
+    print("Check: $type");
+    bool event = await CustomPermissionRequest.check(type);
+    print("Check Value: $event");
     CustomNavigator.showCustomAlertDialog(
-      context, "Xin quyền",
-      title: "Vị trí",
-      enableCancelButton: true,
-      textSubmitted: "Ok",
-      onSubmitted: () {
-        CustomNavigator.pop(context);
-        // PermissionRequest.openSetting();
-      },
+      context,
+      event ? "Allowed" : "Dinied",
     );
   }
 
-
-  Future<bool> _checkBackgroundLocationPermission() async {
-    if (Platform.isAndroid) {
-      return CustomPermissionRequest.check(
-          PermissionRequestType.BACKGROUND_LOCATION);
-    } else {
-      return CustomPermissionRequest.check(PermissionRequestType.LOCATION,
-          checkAlways: true);
-    }
-  }
-
-  _checkLocationPermission() async {
-    bool value = await CustomPermissionRequest.check(
-      PermissionRequestType.LOCATION,
+  _request(PermissionRequestType type) async {
+    print("Request: $type");
+    bool event = await CustomPermissionRequest.request(context, type,
+        onDontAskAgain: () {
+      CustomNavigator.showCustomAlertDialog(
+        context,
+        "onDontAskAgain",
+      );
+    });
+    print("Request Value: $event");
+    CustomNavigator.showCustomAlertDialog(
+      context,
+      event ? "Allowed" : "Dinied",
     );
-
-    if (value) {
-      value = await _checkBackgroundLocationPermission();
-
-      if (!value) {
-        await CustomNavigator.showCustomAlertDialog(
-            context, "back ground request",
-            textSubmitted: "ok",
-            cancelable: false,
-            onSubmitted: () => CustomNavigator.pop(context, object: true));
-
-        _requestBackgroundLocationPermission();
-      } else {
-        init();
-      }
-    } else {
-      await CustomNavigator.showCustomAlertDialog(
-          context, "back ground request",
-          textSubmitted: "ok",
-          cancelable: false,
-          onSubmitted: () => CustomNavigator.pop(context, object: true));
-
-      await CustomPermissionRequest.request(
-          context, PermissionRequestType.LOCATION, onDontAskAgain: () {
-        // PermissionRequest.openSetting();
-      });
-
-      _checkLocationPermission();
-    }
   }
-
-
-
-  Future<void> init() async {
-    print("heelolllloooo");
-  }
-
-
-  _requestBackgroundLocationPermission() async {
-    if (Platform.isAndroid) {
-      bool value = await CustomPermissionRequest.request(
-          context, PermissionRequestType.BACKGROUND_LOCATION,
-          onDontAskAgain: () => (){});
-      if (!value) {
-        _checkLocationPermission();
-      }
-    } else {
-      // PermissionRequest.openSetting();
-      _checkLocationPermission();
-    }
-  }
-
-
-
 }
